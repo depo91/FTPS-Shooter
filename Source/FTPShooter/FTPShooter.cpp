1dc4f06efafdc1093576ab2692d5f5aa1f359479
depo91 <depo991@outlook.com>
2026-08-08 16:51:01 +0200

    .

--- Source/FTPShooter/FTPShooter.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "FTPShooter.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FTPShooter, "FTPShooter" );
-
-DEFINE_LOG_CATEGORY(LogFTPShooter)
